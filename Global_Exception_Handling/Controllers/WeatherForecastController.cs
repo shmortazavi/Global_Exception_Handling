5cb6b699115f5e46118250417723735eafd15dae
shohrehmortazavi <shmo81@gmail.com>
2021-10-19 00:12:37 +0330

    First commit

---
+++ Global_Exception_Handling/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Global_Exception_Handling.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    var rng = new Random();
+        //    var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //        .ToArray();
+        //    return Ok(result);
+        //}
+
+        [HttpGet]
+        public IActionResult Get(string summary)
+        {
+            if (string.IsNullOrWhiteSpace(summary))
+               throw new Exception("sumarry is empty!");
+
+            var result = Summaries.Where(x => x.Contains(summary));
+          
+            if (result == null)
+                return NotFound("summary no found!");
+            else
+                return Ok();
+        }
+    }
+}
---
+++ Global_Exception_Handling/Extensions/CustomExceptionExtention.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Global_Exception_Handling.Extensions
+{
+    public static class CustomExceptionExtention
+    {
+        public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(
+                appError =>
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(new
+                        {
+                            context.Response.StatusCode,
+                            contextFeature.Error.Message
+                        }));
+                    }
+                }));
+        }
+    }
+}
